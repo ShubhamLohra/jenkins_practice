9e3653730adfbb40f0f617b6673dc95222c66732
Shubham-sken <shubhamlohra@salesken.ai>
2023-12-13 13:11:28 +0530

    Update main.java
    added a new line to check build

--- main.java
+++ main.java
@@ -7,6 +7,7 @@ public class main{
         System.out.println("This is output of the java program");
         System.out.println(new Date(0));
         System.out.println("Hello");
+        System.out.println("This is for checking build");
     }
 
 
